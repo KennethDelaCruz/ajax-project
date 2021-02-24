945928b7764d69d2babe4212ea7bf8ee46475c3d
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-02-23 19:33:52 -0800

    NOT COMPLETED:Created a function that will create dom elements taht will have values of the pokemon that was searched

--- js/main.js
+++ js/main.js
@@ -31,6 +31,53 @@ function submitForm() {
   searchForm.reset();
 }
 
- //
+function generatePokemon(object){
+  var mainDiv = document.createElement('div');
+  mainDiv.setAttribute('class', 'selected-pokemon');
+
+  var div1 = document.createElement('div');
+  div1.setAttribute('class', 'column-half poke-picture');
+
+  var pokeImg = document.createElement('img');
+  pokeImg.setAttribute('class', 'poke-img');
+  pokeImg.setAttribute('src', object);
+
+  var div2 = document.createElement('div');
+  div2.setAttribute('class', 'column-half poke-text');
+
+  var subDiv = document.createElement('div');
+  subDiv.setAttribute('class', 'pokemon-details')
+
+  var $name = document.createElement('h3');
+  $name.setAttribute('class', 'pokemon-name');
+  $name.textContent = object.name
+
+  var $type = document.createElement('h4');
+  $type.textContent = 'Type: '+object.types[0].type.name;
+
+  var $number = document.createElement('h4');
+  $number.textContent = '#' + object.id;
+
+  var $description = document.createElement('p');
+  $description.setAttribute('class', 'pokemon-description')
+  $description =
+
+}
+
+// <div class="selected-pokemon">
+  // <div class="column-half poke-picture">
+  //  <img class="poke-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/27.png">
+  //        </div>
+  //  <div class="column-half poke-text">
+  //    <div class="pokemon-details">
+  //      <h3 class="pokemon-name">Sandshrew</h3>
+  //      <h4>Type: Ground</h4>
+  //      <h4>#027</h4>
+  //      <h4>Weight: 12.kg</h4>
+  //    </div>
+  //    <p class="pokemon-description">Burrows deep underground in arid locations far from water. It only emerges to hunt for food.</p>
+  //  </div>
+// </div>
+//
 
 searchForm.addEventListener('submit', submitForm);
